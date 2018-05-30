ee71abb12879ac437532c1039b43662acafb15a5
frank <leaf7639@126.com>
2018-05-30 20:26:12 +0800

    终于调出了像样的色彩渐变实例 开始重新制作PatternPainter

--- files.js
+++ files.js
@@ -7,3 +7,14 @@ var files = {
 		"ColorGradients",
 	]
 };
+
+// PlaneBump ：使用自定义的方式生成一个面片，并且做出形变
+
+
+// trials: 这个目录下面的文件都是一些阶段性尝试
+
+// PolyhedronGeometry ： 投影多边形几何体
+
+// parametricGeometry : 尝试参数化几何体
+
+// ColorGradients-05 : 尝试色彩渐变风格，思路是，先做一个面片，然后手动调节各个顶点的颜色，然后使用各种自定义方法去做出变形，然后用着色器去修正色彩
---
+++ paints.js
@@ -0,0 +1,27 @@
+var paints = {
+
+	'ColorGradients' : {
+
+		name : 'Color Gradients',
+		url : 'ColorGradients.html',
+		picurl : 'images/color-gradients.png'
+
+	},
+
+	'ColorfulBalls' : {
+
+		name: 'Colorful Balls',
+		url : 'ColorGradients.html',
+		picurl : 'images/colorful-balls.png'
+
+	},
+
+	'GridObjects' : {
+
+		name : 'Grid Objects',
+		url : 'ColorGradients.html',
+		picurl : 'images/grid-objects.png'
+
+	}
+
+}
