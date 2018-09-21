9150519ef63851282773d1089891cf91f803d8b5
frank <leaf7639@126.com>
2018-09-21 17:41:57 +0800

    rebuild homepage and add some paints

--- js/_custom/Grid.js
+++ js/_custom/Grid.js
@@ -1,17 +1,17 @@
-function Grid( x, y, z, unitsize_width, unitsize_height, unitsize_depth ) {
-	this.x = x;
-	this.y = y;
-	this.z = z;
+function Grid( xnum, ynum, znum, unitsize_width, unitsize_height, unitsize_depth ) {
+	this.xnum = xnum;
+	this.ynum = ynum;
+	this.znum = znum;
 
 	this.unitsize_width = unitsize_width;
 	this.unitsize_height = unitsize_height;
 	this.unitsize_depth = unitsize_depth;
 
-	this.totalNum = this.x * this.y * this.z;
+	this.totalNum = this.xnum * this.ynum * this.znum;
 
-	this.totalWidth = (this.x-1)*unitsize_width;
-	this.totalHeight = (this.y-1)*unitsize_height;
-	this.totalDepth = (this.z-1)*unitsize_depth;
+	this.totalWidth = (this.xnum-1)*unitsize_width;
+	this.totalHeight = (this.ynum-1)*unitsize_height;
+	this.totalDepth = (this.znum-1)*unitsize_depth;
 
 	this.matrix = [];
 
@@ -19,15 +19,15 @@ function Grid( x, y, z, unitsize_width, unitsize_height, unitsize_depth ) {
 }
 
 Grid.prototype.make = function() {
-	for( var d=0; d<this.z; ++d ) {
-		for( var v=0; v<this.y; ++v ) {
-			for( var h=0; h<this.x; ++h ) {
+	for( var d=0; d<this.znum; ++d ) {
+		for( var v=0; v<this.ynum; ++v ) {
+			for( var h=0; h<this.xnum; ++h ) {
 
-				var x = h * this.unitsize_width - this.totalWidth/2 ;
-				var y = v * this.unitsize_height - this.totalHeight/2;
-				var z = d * this.unitsize_depth - this.totalDepth/2;
+				var xnum = h * this.unitsize_width - this.totalWidth/2 ;
+				var ynum = v * this.unitsize_height - this.totalHeight/2;
+				var znum = d * this.unitsize_depth - this.totalDepth/2;
 
-				var pos = new THREE.Vector3( x, y, z );
+				var pos = new THREE.Vector3( xnum, ynum, znum );
 
 				this.matrix.push(pos);
 			}
--- paints.js
+++ paints.js
@@ -8,21 +8,52 @@ var paints = {
 
 	},
 
-	'ColorfulBalls' : {
+	// floating space
 
-		name: 'Colorful Balls',
-		url : 'ColorfulBalls.html',
-		picurl : 'images/colorful-balls.png'
+	'FloatingSpace_00' : {
+
+		name: 'Floating Space',
+		url : 'FloatingSpace_00.html',
+		picurl : 'images/floating-space-00.png'
 
 	},
+
+	'FloatingSpace_01' : {
+
+		name: 'Floating Space #02',
+		url : 'FloatingSpace_01.html',
+		picurl : 'images/floating-space-01.png'
+
+	},
+
+	// scifi dots
 
 	'Scifidots' : {
 
 		name : 'Sci-fi dots',
 		url : 'scifidots_dof.html',
-		picurl : 'images/grid-objects.png'
+		picurl : 'images/scifi-dots.png'
 
 	},
+
+	'Scifidots_02' : {
+
+		name : 'Sci-fi dots #02',
+		url : 'scifidots.html',
+		picurl : 'images/scifi-dots-2.png'
+
+	},
+
+
+	'Scifidots_03' : {
+
+		name : 'Sci-fi dots #03',
+		url : 'scifidots_wavedots.html',
+		picurl : 'images/scifi-dots-3.png'
+
+	},
+
+
 
 
 }
